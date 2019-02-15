f05654d488dd76a8c49cbc891d81245c9927af83
kimseungki94 <skkim0832@gmail.com>
2019-02-15 23:21:35 +0900

    static portfolio

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 import blogapp.views
+import portfolio.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',blogapp.views.home, name='home'),
     path('blog/',include('blogapp.urls')),
-    
+    path('portfolio/',portfolio.views.portfolio,name="portfolio"),
 ]
 
