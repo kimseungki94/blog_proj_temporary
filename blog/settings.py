f844948a42f7cdbf9dcf2c96f5b8fabfbf0215f9
kimseungki94 <skkim0832@gmail.com>
2019-02-16 09:47:21 +0900

    media function portfolio

--- blog/settings.py
+++ blog/settings.py
@@ -126,3 +126,6 @@ STATICFILES_DIRS = [
 ] #static 파일들이 현재 어디에 있는지를 쓰는 곳!
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # static 파일들이 어디로 모일 것인지를 쓰는 곳!
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 import blogapp.views
 import portfolio.views
+from django.conf import settings #암기 미디어파일 사용하기 위해서1
+from django.conf.urls.static import static #암기 미디어파일 사용하기 위해서2
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',blogapp.views.home, name='home'),
     path('blog/',include('blogapp.urls')),
     path('portfolio/',portfolio.views.portfolio,name="portfolio"),
-]
-
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+# + 이후에 있는거는 암기
+#OR urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 로 써도됨
