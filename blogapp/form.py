5e69adbf433a15c169db8d5f615944a54bf366b9
kimseungki94 <skkim0832@gmail.com>
2019-02-18 14:55:43 +0900

    push

---
+++ blogapp/form.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Blog
+
+class BlogPost(forms.ModelForm):
+   
+#모델py에 블로그 객체를 받기위해서는 이렇게 forms.ModelForm 사용
+    class Meta:
+        model = Blog
+        fields = ['title','body']
+
+# forms.Form
+#  email = forms.EmailField()
+#     files = forms.FileField()
+#     url = forms.URLField()
+#     words = forms.CharField(max_length=200)
+#     max_number = forms.ChoiceField(choices=[('1','one'),('2','two'),('3','three')])
